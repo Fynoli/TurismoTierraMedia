aa37850bf9ec157c60a76af346dad16cc4e1be1c
flopy1993 <flopy1993@gmail.com>
2020-11-20 14:00:20 -0300

    modificacion de BD

--- TurismoEnLaTierraMedia/src/Basedatos.java
+++ TurismoEnLaTierraMedia/src/Basedatos.java
@@ -5,9 +5,6 @@ public class Basedatos {
 
     public static void main(String [] args){
     	
-    	
-    	
-
         try{
 
             //1 - Crear la conexion, Obj Connection
@@ -17,11 +14,11 @@ public class Basedatos {
             // 2 - Crear un objeto Statement
             Statement miStatement = miConexion.createStatement();
             // 3 - Ejecutar una query SQL
-            ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM usuarios");
+            ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM usuario");
             System.out.println("envio query");
             // 4 - Leer el ResultSet, que seria el resultado de la consulta
             while(miResultSet.next()){
-                System.out.println(miResultSet.getString("Nombre") + ' ' + miResultSet.getString("Mail"));
+                System.out.println(miResultSet.getString("nombre") + ' ' + miResultSet.getInt("presupuesto"));
             }
 
 
